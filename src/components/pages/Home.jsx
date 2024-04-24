beeb6f8873c3b7df702cdb059a6a05451b2ef7dc
davidfrk <davida1d2@gmail.com>
2024-04-24 16:45:42 -0300

    Add image showcase component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,20 @@
-import Loading from "../layout/Loading"
+import ImageShowcase from '../display/ImageShowcase'
+
+const images = [
+  "/src/images/image0.jpg",
+  "/src/images/image1.jpg",
+  "/src/images/image2.jpg",
+  "/src/images/image3.jpg",
+  "/src/images/image4.jpg",
+  "/src/images/image5.jpg"
+]
 
 function Home() {
   return (
-    <div>
-      Home
-      <Loading />
+    <div className='w-full overflow-hidden bg-slate-800'>
+      <div className='w-full h-full flex justify-center items-center'>
+        <ImageShowcase images={images} />
+      </div>
     </div>
   )
 }
