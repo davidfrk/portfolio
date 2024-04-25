372d29aab3f17e538c2894071919947653866f3b
davidfrk <davida1d2@gmail.com>
2024-04-25 09:05:44 -0300

    Add text description to item showcase, updating style

---
+++ src/components/display/ItemShowcase.jsx
@@ -0,0 +1,65 @@
+import { useRef, useEffect, useState } from 'react'
+import { IoIosArrowDropright, IoIosArrowDropleft } from "react-icons/io";
+
+import styles from './ItemShowcase.module.css'
+
+function ItemShowcase({ items }) {
+  const container = useRef(null)
+  const [firstElement, setFirstElement] = useState()
+
+  useEffect(() => {
+    const first = container.current.firstElementChild
+    first.classList.add(styles.firstElement)
+    setFirstElement(first)
+  }, [])
+
+  function next() {
+    container.current.append(container.current.firstElementChild)
+    const lastElement = container.current.lastElementChild
+    
+    lastElement.classList.add(styles.slide_to_last)
+    setTimeout(() => {
+      lastElement.classList.remove(styles.slide_to_last)
+    }, 800);
+
+    updateFirstElement()
+  }
+
+  function prev() {
+    container.current.prepend(container.current.lastElementChild)
+    updateFirstElement()
+  }
+
+  function updateFirstElement() {
+    firstElement.classList.remove(styles.firstElement)
+    const new_first = container.current.firstElementChild
+    new_first.classList.add(styles.firstElement)
+    setFirstElement(new_first)
+  }
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div className='flex flex-col pb-[120px]'>
+      <div ref={container} className={styles.container} style={{}}>
+        {items && items.map((item, index) => (
+          <div className={styles.item} key={index}>
+            <img src={item.image} alt={item.alt} className="w-full h-full object-cover"/>
+            <div className={styles.textContainer}>
+              <h1>{item.title}</h1>
+              <p className={styles.description}>{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className='w-1/2 flex gap-4 mt-4 justify-center z-10'>
+        <button className="rounded-full text-[64px] text-slate-300 hover:text-white" type="button" onClick={prev} > <IoIosArrowDropleft /> </button>
+        <button className="rounded-full text-[64px] text-slate-300 hover:text-white" type="button" onClick={next} > <IoIosArrowDropright /> </button>
+      </div>
+    </div>
+  )
+}
+
+export default ItemShowcase
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,19 +1,19 @@
-import ImageShowcase from '../display/ImageShowcase'
+import ItemShowcase from '../display/ItemShowcase'
 
-const images = [
-  "/src/images/image0.jpg",
-  "/src/images/image1.jpg",
-  "/src/images/image2.jpg",
-  "/src/images/image3.jpg",
-  "/src/images/image4.jpg",
-  "/src/images/image5.jpg"
+const items = [
+  {image: "/src/images/image0.jpg", alt:"showcase", title:"Sítio - Igarapé", description:"R$680.000"},
+  {image: "/src/images/image1.jpg", alt:"showcase", title:"Sítio - Belo Horizonte", description:"R$680.000"},
+  {image: "/src/images/image2.jpg", alt:"showcase", title:"Sítio - Igarapé", description:"R$680.000"},
+  {image: "/src/images/image3.jpg", alt:"showcase", title:"Sítio - Igarapé", description:"R$680.000"},
+  {image: "/src/images/image4.jpg", alt:"showcase", title:"Sítio - Igarapé", description:"R$680.000"},
+  {image: "/src/images/image5.jpg", alt:"showcase", title:"Sítio - Igarapé", description:"R$680.000"},
 ]
 
 function Home() {
   return (
     <div className='w-full overflow-hidden bg-slate-800'>
       <div className='w-full h-full flex justify-center items-center'>
-        <ImageShowcase images={images} />
+        <ItemShowcase items={items} />
       </div>
     </div>
   )
