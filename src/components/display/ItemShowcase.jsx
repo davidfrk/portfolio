f5f5eba6b3d60bcd4f44efacf30a93585ff0808f
davidfrk <davida1d2@gmail.com>
2024-04-27 06:10:33 -0300

    Providing aria-label for prev and next buttons, using dynamic width for item cards

--- src/components/display/ItemShowcase.jsx
+++ src/components/display/ItemShowcase.jsx
@@ -56,8 +56,18 @@ function ItemShowcase({ items }) {
           ))}
         </div>
         <div className='w-1/2 flex gap-4 mt-4 justify-center z-10'>
-          <button className="rounded-full text-[64px] text-slate-300 hover:text-[var(--accent-color)]" type="button" onClick={prev} > <IoIosArrowDropleft /> </button>
-          <button className="rounded-full text-[64px] text-slate-300 hover:text-[var(--accent-color)]" type="button" onClick={next} > <IoIosArrowDropright /> </button>
+          <button
+            className="rounded-full text-[64px] text-slate-300 hover:text-[var(--accent-color)]"
+            type="button" onClick={prev} aria-label="Ir para item anterior"
+          >
+              <IoIosArrowDropleft />
+            </button>
+          <button
+            className="rounded-full text-[64px] text-slate-300 hover:text-[var(--accent-color)]" 
+            type="button" onClick={next} aria-label="Ir para próximo item"
+          >
+            <IoIosArrowDropright />
+          </button>
         </div>
       </div>
     </div>
